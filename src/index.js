ccc53f2e5c2314454566903194b375e5f1fceb5c
ElenaZhukouskaya <elenazhukouskaya@gmail.com>
2022-02-23 13:04:46 +0300

    feat: add function toReadable(number)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const digital = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const dozens = [
+        "",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number === 0) {
+        return "zero";
+    }
+    if (number > 0 && number < 20) {
+        return digital[number];
+    }
+    if (number >= 20 && number < 100) {
+        return number % 10 === 0
+            ? dozens[number / 10]
+            : dozens[Math.trunc(number / 10)] + " " + digital[number % 10];
+    }
+
+    if (number >= 100 && number < 1000) {
+        return number % 100 === 0
+            ? digital[number / 100] + " hundred"
+            : digital[Math.trunc(number / 100)] +
+                  " hundred " +
+                  toReadable(number - Math.trunc(number / 100) * 100);
+    }
+};
